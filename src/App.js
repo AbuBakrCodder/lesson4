d69041d23a658a0cdd4371c7ddb813cbe0f5a23f
Abu Bakr <abubakrprogrammer11@gmail.com>
2025-09-21 20:09:51 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { use, useState } from 'react';
 
 function App() {
+  let [users, setUser] = useState(
+    [
+      {
+        name: "Bmw",
+        id: 1
+      },
+      {
+        name: "Mercedes-Benz",
+        id: 2
+      },
+      {
+        name: "Audi",
+        id: 3
+      },
+      {
+        name: "Porsche",
+        id: 4
+      }
+    ]
+  )
+
+  let h2 = document.querySelector("h2")
+
+  function deletebtn(id) {
+    const filter = users.filter(user => {
+      return user.id !== id
+    })
+
+    if (filter.length === 0) {     
+      setUser(filter)
+      h2.classList.toggle("hidden")
+    }
+    else {
+      setUser(filter)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>
+        German cars
+      </h1>
+      <h2 className='hidden'>No cars</h2>
+      {
+        users.map((user) => {
+          return (
+            <div key={user.id} className='cars'>
+              <h1>
+                {user.name}
+              </h1>
+                <button onClick={() => { deletebtn(user.id) }}>Delete</button>
+            </div>
+          )
+        })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
